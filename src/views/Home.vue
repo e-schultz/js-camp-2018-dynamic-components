996977c399324706db7b0ed8a8b347cc741134b9
Evan Schultz <evan@rangle.io>
2018-07-19 20:40:23 +0200

    links

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,15 @@
 <template>
-    <section>      
-      
+    <section class="section">      
+      <ul>
+          <li> <a href="https://github.com/e-schultz/js-camp-2018-dynamic-components">Github Repo: e-schultz/js-camp-2018-dynamic-components</a></li>
+          <li> <a href="https://js-camp-2018-dynamic-demo.now.sh/#/">Live Demo</a></li>
+          <li> <a href="https://blog.rangle.io/how-to-create-data-driven-user-interfaces-in-vue/">Blog Post / Tutorial</a></li>
+      </ul>
+
+      <!--
       <contact-details v-model="contact"></contact-details>
       <address-details v-model="contact"></address-details>
+      -->
     </section>
 </template>
 
